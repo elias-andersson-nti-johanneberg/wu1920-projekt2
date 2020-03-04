599b5c24912534bad9e11b098ad3f8339fb4bc28
elias-andersson-nti-johanneberg <60173037+elias-andersson-nti-johanneberg@users.noreply.github.com>
2020-03-04 11:10:23 +0100

    Iag satta jag in videos, la in smooth scrolling och länkade a element till olika platser på min sida. Därefter la jag in creative och försökte börja mobil anpassa sidan men de gick sådär och sen hade jag ingen tid så jag radera de jobbet.
    videos är för stora för github så vet inte hur jag ska ge dem till dig ola.

--- js/code.js
+++ js/code.js
@@ -5,3 +5,21 @@
     document.getElementById("overlay").style.opacity = 0;
   }
   
+  function bringToFront() {
+    document.getElementById("overlay").style.zIndex = 1;
+  }
+
+  function myFunction() {
+    document.getElementById("overlay").style.zIndex = "1";
+}
+
+function openNav() {
+  document.getElementById("mySidepanel").style.width = "250px";
+}
+
+/* Set the width of the sidebar to 0 (hide it) */
+function closeNav() {
+  document.getElementById("mySidepanel").style.width = "0";
+}
+
+
